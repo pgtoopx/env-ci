d9bf08cd6327f20df2f3663ded0302426e583e72
Eduardo Daniel Salinas Rubio <esalinasr@bancochile.cl>
2020-11-02 16:18:02 -0300

    update to extra host

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd jenkins && make build && make run
+docker rm $(docker ps -a -f status=exited -q)
+cd jenkins && make clean-images
+make build && make run
 cd ../dockprom && ADMIN_USER=admin ADMIN_PASSWORD=admin docker-compose up -d
 
